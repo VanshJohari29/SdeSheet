b0bbdf43425a621b4a37869b6d778520d2457f68
Vansh Johari <71847743+VanshJohari29@users.noreply.github.com>
2022-06-08 11:06:23 +0530

    Create sortColors.cpp

---
+++ sortColors.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        
+        int lo = 0; 
+        int hi = nums.size() - 1; 
+        int mid = 0; 
+
+        while (mid <= hi) { 
+            switch (nums[mid]) { 
+
+            // If the element is 0 
+            case 0: 
+                swap(nums[lo++], nums[mid++]); 
+                break; 
+
+            // If the element is 1 . 
+            case 1: 
+                mid++; 
+                break; 
+
+            // If the element is 2 
+            case 2: 
+                swap(nums[mid], nums[hi--]); 
+                break; 
+            }
+        }
+    }
+};
+
+// Here we used switch because we know there is only 3 elements 
+// here we have used 3 pointer approach 
+//this problem is a variation of popular dutch national flag algorithm
